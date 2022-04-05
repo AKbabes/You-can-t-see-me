ec855f7cf09051c5c7b71df690d62816d0c7f3f7
Asif Akram <97173662+AKbabes@users.noreply.github.com>
2022-04-05 19:04:16 +0600

    project updated

---
+++ SP2_Project_Quote Generator/script.js
@@ -0,0 +1,30 @@
+const quoteText = document.querySelector(".quote"),
+    quoteBtn = document.querySelector("button"),
+    authorName = document.querySelector(".name"),
+    twitterBtn = document.querySelector(".twitter"),
+    copyBtn = document.querySelector(".copy"),
+
+    synth = speechSynthesis;
+
+function randomQuote() {
+    quoteBtn.classList.add("loading");
+    quoteBtn.innerText = "Loading Quote...";
+    fetch("http://api.quotable.io/random").then(response => response.json()).then(result => {
+        quoteText.innerText = result.content;
+        authorName.innerText = result.author;
+        quoteBtn.classList.remove("loading");
+        quoteBtn.innerText = "New Quote";
+    });    
+    
+}
+
+twitterBtn.addEventListener("click", () => {
+    let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.innerText}`;
+    window.open(tweetUrl, "_blank");
+});
+
+copyBtn.addEventListener("click", () => {
+    navigator.clipboard.writeText(quoteText.innerText);
+});
+
+quoteBtn.addEventListener("click", randomQuote);
